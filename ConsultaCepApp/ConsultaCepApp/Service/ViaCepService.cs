f3edf2acccb3de2f85a6a37b681cd100f86047bf
Eduardo A. Cruz Junior <eduardo_acj2@hotmail.com>
2020-07-23 09:16:31 -0300

    Adicionar arquivos de projeto.

---
+++ ConsultaCepApp/ConsultaCepApp/Service/ViaCepService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsultaCepApp.Service {
+    public class ViaCepService {
+        private static string URL = "https://viacep.com.br/ws/{0}/json/";
+
+    }
+}
